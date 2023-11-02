67ef655382049b499b271c8fd171dbf8eda765e1
DThongLeon <114857346+DThongLeon@users.noreply.github.com>
2023-11-02 15:52:54 +0700

    Cusstomer

--- WebAPI/Controllers/CustomerController.cs
+++ WebAPI/Controllers/CustomerController.cs
@@ -2,16 +2,11 @@
 using Application.Services.Implementations;
 using Application.Services.Interfaces;
 using Domain.Entities;
-<<<<<<< Updated upstream
-using Infrastructure.Common;
-using Infrastructure.DTOs.Request.Customer;
-=======
 using Domain.Enums;
 using Infrastructure.Common;
 using Infrastructure.DTOs.Request.Customer;
 using Infrastructure.DTOs.Response;
 using Infrastructure.DTOs.Response.Customer;
->>>>>>> Stashed changes
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -19,11 +14,7 @@ namespace WebAPI.Controllers
 {
     /// <summary>
     /// </summary>
-<<<<<<< Updated upstream
-    [Route("api/[controller]")]
-=======
     [Route("api/v1/customer")]
->>>>>>> Stashed changes
     [ApiController]
     public class CustomerController : ControllerBase
     {
@@ -34,16 +25,6 @@ namespace WebAPI.Controllers
  
             this.customerService = customerService;
         }
-<<<<<<< Updated upstream
-
-
-        [HttpGet]
-        public async Task<PagedList<Customer>> GetAllCustomer(int pageSize, int pageNumber)
-        {
-            try
-            {
-               return await customerService.GetAll(pageSize, pageNumber);
-=======
         /// <summary>
         /// Get paginated list of Customer
         /// </summary>
@@ -63,15 +44,12 @@ namespace WebAPI.Controllers
                     Message = "Successfully retrieved paginated list of Customer",
                     Data = result
                 });;
->>>>>>> Stashed changes
             }
             catch
             {
                 return null;
             }
         }
-<<<<<<< Updated upstream
-=======
         /// <summary>
         /// Add new Customer
         /// </summary>
@@ -79,15 +57,11 @@ namespace WebAPI.Controllers
         /// <returns></returns>
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ResponseObject<Customer>))]
         /// 
->>>>>>> Stashed changes
         [HttpPost]
         public async Task<ActionResult<Customer>> PostCustomer([FromBody] CustomerRequest customer)
         {
             try
             {
-<<<<<<< Updated upstream
-                return Ok(await customerService.AddNewCustomer(customer));
-=======
                 var result = await customerService.AddNewCustomer(customer);
                 return Ok(new ResponseObject<Customer>()
                 {
@@ -95,55 +69,33 @@ namespace WebAPI.Controllers
                     Message = "Successfully add new Customer",
                     Data = result
                 }); ;
->>>>>>> Stashed changes
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
-<<<<<<< Updated upstream
-=======
         /// <summary>
         /// Update current Customer
         /// </summary>
         /// <param name="customer">Parameters for Customer information need update</param>
         /// <returns></returns>
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ResponseObject<Customer>))]
->>>>>>> Stashed changes
         [HttpPut]
         public async Task<IActionResult> PutUpdateCustomer([FromBody] UpdateCustomerRequest customer)
         {
             try { 
             customerService.UpdateCustomer(customer);
-<<<<<<< Updated upstream
-            return Ok();
-=======
                 return Ok(new ResponseObject<Customer>()
                 {
                     Status = ResponseStatus.Success.ToString(),
                     Message = "Successfully update Customer",
                 }); ;
->>>>>>> Stashed changes
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
-<<<<<<< Updated upstream
-        } 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Customer>> GetCustomerByID([FromRoute] int id)
-        {
-            var customer = await customerService.GetCustomerByID(id);
-
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            return customer;
-=======
         }
         /// <summary>
         /// Search Curtomer By Id
@@ -195,7 +147,6 @@ namespace WebAPI.Controllers
                 return BadRequest(ex.Message);
             }
 
->>>>>>> Stashed changes
         }
     }
 }
