54dc3a51319763412e471c2b864aaf6b3a528ebc
hieunghia489 <hieunghia489@gmail.com>
2023-11-07 22:25:59 +0700

    done all (chua phan page)

--- Infrastructure/DTOs/Response/Staff/StaffProfileResponse.cs
+++ Infrastructure/DTOs/Response/Staff/StaffProfileResponse.cs
@@ -11,7 +11,6 @@ namespace Infrastructure.DTOs.Response.Staff
     {
         public required int Id { get; set; }
         public required string Username { get; set; }
-        public required string Password { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string PhoneNumber { get; set; }
