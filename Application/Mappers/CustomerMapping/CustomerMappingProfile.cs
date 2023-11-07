81a081aab50170efcb4f7407fd8e92e3d053c8fc
DThongLeon <114857346+DThongLeon@users.noreply.github.com>
2023-11-07 19:27:37 +0700

    Update Id customer and format datetime

--- Application/Mappers/CustomerMapping/CustomerMappingProfile.cs
+++ Application/Mappers/CustomerMapping/CustomerMappingProfile.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Infrastructure.Common;
+using Infrastructure.Utils;
 
 namespace Application.Mappers.CustomerMapping
 {
@@ -14,7 +15,15 @@ namespace Application.Mappers.CustomerMapping
     {
         public CustomerMappingProfile()
         {
-            CreateMap<Customer, CustomerResponse>();
+            CreateMap<Customer, CustomerResponse>().ForMember(dest => dest.CreateDate,
+                src => src.MapFrom(src => DateTimeHelper.FormatDateTimeToDatetime(src.CreateDate)))
+                  .ForMember(dest => dest.UpdateDate,
+                src => src.MapFrom(src => DateTimeHelper.FormatDateTimeToDatetime(src.UpdateDate)))
+                     .ForMember(dest => dest.DateOfBirth,
+                src => src.MapFrom(src => DateTimeHelper.FormatDateTimeToDatetime(src.DateOfBirth)))
+                .ForMember(dest => dest.Status,
+                src => src.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.CustomerId, src => src.MapFrom(src => src.Id));
             CreateMap<PagedList<Customer>, PagedList<CustomerResponse>>();
         }
     }
--- Infrastructure/DTOs/Response/Customer/CustomerResponse.cs
+++ Infrastructure/DTOs/Response/Customer/CustomerResponse.cs
@@ -5,17 +5,17 @@ namespace Infrastructure.DTOs.Response.Customer
 {
     public class CustomerResponse
     {
-        public int CustomerId { get; }
+        public int CustomerId { get; set; }
         public CustomerStatus Status { get; set; }
         public  string FirstName { get; set; }
         public  string LastName { get; set; }
         public  string PhoneNumber { get; set; }
-        public DateTime? DateOfBirth { get; set; }
+        public string? DateOfBirth { get; set; }
         public Gender? Gender { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
 
-        public DateTime CreateDate { get; set; }
-        public DateTime UpdateDate { get; set; }
+        public string? CreateDate { get; set; }
+        public string? UpdateDate { get; set; }
     }
 }
